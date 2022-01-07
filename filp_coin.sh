af7e7ec5eab9dc00bf046335955bc120b8f59f54
neelu <neelimatalluri98@gmail.com>
2022-01-07 16:49:03 +0530

    neelima uc2

--- filp_coin.sh
+++ filp_coin.sh
@@ -1,11 +1,34 @@
-#! /bin/bash
+#! /bin/bash -x
+
+declare -A flipCoin
+read -p "How many times you want to flip coin " noOfTime
 
 IS_HEAD=1
-flipCoinResult=$((RANDOM%2))
+counter=0
+headCount=0
+tailCount=0
 
-if [ $flipCoinResult -eq $IS_HEAD ]
-then
-        echo "H"
-else
-        echo "T"
-fi
+while [ $counter -lt $noOfTime ]
+do
+        flipCoinResult=$((RANDOM%2))
+        if [ $flipCoinResult -eq $IS_HEAD ]
+        then
+                echo "Head"
+                flipCoin[$counter]="Head"
+                ((headCount++))
+        else
+                echo "Tail"
+                flipCoin[$counter]="Tail"
+                ((tailCount++))
+        fi
+((counter++))
+done
+
+
+#Percentage of  head
+percnetageOfHead=$(((($headCount)*100)/$noOfTime))
+echo "Percentage of head " $percnetageOfHead
+
+#Percentage of tail
+percentageofTail=$((100-$percnetageOfHead))
+echo "Percentage of tail "$percentageofTail
